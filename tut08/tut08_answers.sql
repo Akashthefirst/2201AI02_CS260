0802f231c0761fe447f41e5920b064f329136943
Akashthefirst <118078964+Akashthefirst@users.noreply.github.com>
2024-04-16 00:05:36 +0530

    Update tut08_answers.sql

--- tut08/tut08_answers.sql
+++ tut08/tut08_answers.sql
@@ -1,7 +1,10 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
+-- Akash Sinha
+-- 2201AI02
 
+use tut8;
 ---1.
 DELIMITER //
 CREATE TRIGGER IncreaseSalaryTrigger
